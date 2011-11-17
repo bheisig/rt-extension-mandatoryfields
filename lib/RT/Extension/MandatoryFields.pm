289a6cdd78e287553aede510d992ee1ec3e58d4e
Benjamin Heisig <benjamin@heisig.name>
2011-11-17 16:31:13 +0100

    small improvements concerning meta information

--- lib/RT/Extension/MandatoryFields.pm
+++ lib/RT/Extension/MandatoryFields.pm
@@ -10,17 +10,12 @@ RT::Extension::MandatoryFields - Enforce users to fill standard fields when
 creating a ticket
 
 
-=head1 VERSION
-
-Version 0.2
-
-
 =cut
 
 our $VERSION = '0.2';
 
 
-=head1 SYNOPSIS
+=head1 DESCRIPTION
 
 This RT Extension enforces users to fill standard fields defined in RT site
 configuration file when creating a ticket via the web interface.
@@ -28,7 +23,7 @@ configuration file when creating a ticket via the web interface.
 
 =head1 AUTHOR
 
-Benjamin Heisig, C<< <bheisig at synetics.de> >>
+Benjamin Heisig, E<lt>bheisig@synetics.deE<gt>
 
 
 =head1 SUPPORT AND DOCUMENTATION
@@ -40,21 +35,21 @@ You can find documentation for this module with the perldoc command.
 
 =head1 BUGS
 
-Please report any bugs or feature requests to the AUTHOR.
+Please report any bugs or feature requests to the L<author|/"AUTHOR">.
 
 
 =head1 ACKNOWLEDGEMENTS
 
-This extension is a fork of RT::Extension::MandatorySubject written by
+This extension is a fork of L<RT::Extension::MandatorySubject> written by
 Emmanuel Lacour.
 
 Special thanks to the synetics GmbH, C<< <http://i-doit.org/> >> for initiating
 this project!
 
 
-=head1 COPYRIGHT & LICENSE
+=head1 COPYRIGHT AND LICENSE
 
-Copyright 2011 Benjamin Heisig <bheisig@synetics.de>
+Copyright 2011 Benjamin Heisig, E<lt>bheisig@synetics.deE<gt>
 
 This program is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
